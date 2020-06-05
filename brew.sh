ce69ce1da29d5bbea43cef91b5cc56cad27d01f7
brianbianchi <bianchibrianj@gmail.com>
2020-06-05 19:10:21 -0400

    htop

--- brew.sh
+++ brew.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-# Install Brew Packages
+# Install brew packages
 brew install python3
 brew install node
 brew install automake
 brew install binutils
+brew install htop
 brew install gcc
 brew install tmux
 brew install vim
 brew install wget
 
-# Install MacOS Applications
+# Install applications
 brew cask install google-chrome
 brew cask install brave-browser
 brew cask install visual-studio-code
 brew cask install spotify
 brew cask install discord
-brew cask install gimp
 brew cask install vlc
 brew cask install postman
