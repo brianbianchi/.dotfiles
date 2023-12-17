c8550c4d42bfbf773fbe3ff1b06f6da2961d081a
brianbianchi <bianchibrianj@gmail.com>
2023-12-16 23:46:06 -0500

    add karabiner-elements and devtoys brew apps

--- brew.sh
+++ brew.sh
@@ -20,11 +20,13 @@ brew install wget
 brew install --cask anaconda
 brew install --cask brave-browser
 brew install --cask burp-suite
+brew install --cask devtoys
 brew install --cask discord
 # brew install --cask docker
 brew install --cask firefox
 brew install --cask google-chrome
 brew install --cask insomnia
+brew install --cask karabiner-elements
 brew install --cask rectangle
 brew install --cask slack
 brew install --cask spotify
