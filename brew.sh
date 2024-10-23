233dadf7ba56394fefe6db3b432ebf1c5af22c9b
brianbianchi <bianchibrianj@gmail.com>
2024-10-22 20:06:09 -0400

    comment karabiner-elements

--- brew.sh
+++ brew.sh
@@ -26,7 +26,7 @@ brew install --cask docker
 brew install --cask firefox
 brew install --cask google-chrome
 brew install --cask insomnia
-brew install --cask karabiner-elements
+# brew install --cask karabiner-elements
 # brew install --cask prince # md preview enhanced
 brew install --cask rectangle
 brew install --cask slack
